8ac9345e61264f1dc61db1d32f7a9a9d7fd132f7
Anny K Fernandes <32650449+Annykfs@users.noreply.github.com>
2019-03-28 16:02:57 -0300

    Add files via upload

---
+++ I1001.java
@@ -0,0 +1,19 @@
+
+package resolucaouri;
+
+import java.io.IOException;
+import java.util.Scanner; 
+
+public class I1001 {
+    public static void main(String[] args) throws IOException {
+		
+		Scanner in = new Scanner(System.in);
+		
+		int A = in.nextInt();
+		int B = in.nextInt();	
+		int x = (A + B);	
+		
+		System.out.println("X = "+x);	
+    }
+ 
+}

567dbc4e419819b8a1617b6eb1553bab1f840f0f
Anny K Fernandes <32650449+Annykfs@users.noreply.github.com>
2019-04-09 02:14:52 -0300

    Update Resolucao1042.java

--- Resolucao1042.java
+++ Resolucao1042.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package resolucaouri;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-/**
- *
- * @author nicod
- */
 public class Resolucao1042 {
-    
+ 
+    public static void main(String[] args) throws IOException {
+ 
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+            String line = reader.readLine();      
+            String array[] = line.split(" ");  
+           
+            int x = 0,y = 0,z = 0;
+            
+            x = Integer.parseInt(array[0]);
+            y = Integer.parseInt(array[1]);
+            z = Integer.parseInt(array[2]);
+            
+            int inx = x;int iny = y;int inz = z;
+            
+            int maior = Math.max(x, Math.max(y, z));
+            int medio = 0, menor = 0;
+            if(x == maior){
+                medio = Math.max(y, z); 
+                menor = Math.min(y, z);
+            }else if(y == maior){
+                medio = Math.max(x, z);
+                menor = Math.min(x, z);
+            } else if (z == maior){
+                medio = Math.max(x, y);
+                menor = Math.min(x, y);
+            }
+            
+            System.out.println(menor);
+            System.out.println(medio);
+            System.out.println(maior);
+            System.out.print("\n");
+            System.out.println(inx);
+            System.out.println(iny);
+            System.out.println(inz);   
+         
+ 
+    }
+ 
 }
